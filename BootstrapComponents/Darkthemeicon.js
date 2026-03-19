cb4e5391ac5db9473798f74335f25d16cd43539f
nivas <nivassri183@gmail.com>
2026-03-19 16:03:10 +0530

    changed a minor bug on when web loads it stayed white theme but when tried to switch it didn't switched but now its switching

--- BootstrapComponents/Darkthemeicon.js
+++ BootstrapComponents/Darkthemeicon.js
@@ -6,11 +6,11 @@ const themeIcon = document.querySelector("#themeIcon");
 
 getDarkThemeData.addEventListener("change", ()=>{
     if(getDarkThemeData.checked){
-        htmlElement.setAttribute('data-bs-theme', 'light');
-        themeIcon.textContent = "☀️";
-    }
-    else{
         htmlElement.setAttribute('data-bs-theme', 'dark');
         themeIcon.textContent = "🌙";
+    }
+    else{
+        htmlElement.setAttribute('data-bs-theme', 'light');
+        themeIcon.textContent = "☀️";
     }
 })
