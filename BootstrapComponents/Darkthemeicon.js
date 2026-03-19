e8c1f8ac1027ce0f1e9fc0b172cb92c7426bfd29
nivas <nivassri183@gmail.com>
2026-03-19 15:51:04 +0530

    added dark theme switch to Bootstrap component program.

---
+++ BootstrapComponents/Darkthemeicon.js
@@ -0,0 +1,16 @@
+
+
+const getDarkThemeData = document.querySelector("#checkNativeSwitch");
+const htmlElement = document.documentElement
+const themeIcon = document.querySelector("#themeIcon");
+
+getDarkThemeData.addEventListener("change", ()=>{
+    if(getDarkThemeData.checked){
+        htmlElement.setAttribute('data-bs-theme', 'light');
+        themeIcon.textContent = "☀️";
+    }
+    else{
+        htmlElement.setAttribute('data-bs-theme', 'dark');
+        themeIcon.textContent = "🌙";
+    }
+})
